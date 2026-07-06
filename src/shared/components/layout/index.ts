6ba7b1a75e615a820729ebd88cdde2603453ee1e
Matthieu <matthieumarin51@gmail.com>
2026-07-06 09:12:52 +0200

    refactor(renderer): remove obsolete code-metaphor primitives, final cleanup

--- src/shared/components/layout/index.ts
+++ src/shared/components/layout/index.ts
@@ -1,9 +1,6 @@
 export { PageShell } from './PageShell';
 export { CodeCard } from './CodeCard';
 export { ClassHeader, ClassBody, ClassClose } from './ClassHeader';
-export { CodeProperty, CodeArrayProperty, CodeArrayItem } from './CodeProperty';
-export { SkillLevel } from './SkillLevel';
-export type { SkillLevelValue } from './SkillLevel';
 export { ACCENT_CLASSES } from './accent';
 export type { AccentColor } from './accent';
 export { useCodeCardAccent } from './CodeCardContext';
